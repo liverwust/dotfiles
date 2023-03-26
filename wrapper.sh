88fca65cee66ae29d5f0c21f245ed3ff9e9337f1
Louis Wust <louis.w.wust@nasa.gov>
2023-03-26 19:23:06 -0400

    Add dircolors and include in wrapper.sh
    This is to allow for better Solarized support with commands such as `ls`

--- wrapper.sh
+++ wrapper.sh
@@ -2,3 +2,7 @@
 # This is for shared user accounts at work.
 
 alias vim="vim -u $HOME/.homesick/repos/dotvim/home/.vimrc"
+
+# https://github.com/liverwust/dotvim
+# https://github.com/sigurdga/ls-colors-solarized/blob/master/dircolors
+eval `dircolors "$HOME/.homesick/repos/dotvim/dircolors"`
