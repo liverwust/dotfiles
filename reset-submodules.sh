fa7462ad56785963fc87de1bf45100fbc6c75d21
Louis Wust <louis.w.wust@nasa.gov>
2024-04-24 14:10:51 -0400

    Don't run git submodule update directly

---
+++ reset-submodules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+MY_PATH=`dirname "$0"`
+test -d "${MY_PATH}/.git" || exit 1
+test -f "${MY_PATH}/reset-submodules.sh" || exit 1
+rm -rf "${MY_PATH}/.git/modules/"*
+rm -rf "${MY_PATH}/vim-"*
+rm -rf "${MY_PATH}/ansible-vim"
+rm -rf "${MY_PATH}/tlib_vim"
