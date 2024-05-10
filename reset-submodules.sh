3c55411a36b23831e79ac0fe003c975d1570e089
Louis Wust <louiswust@fastmail.fm>
2024-05-10 16:10:24 -0400

    Update reset-submodules.sh
    Don't forget to delete the restricted-submodules directory.

--- reset-submodules.sh
+++ reset-submodules.sh
@@ -6,5 +6,6 @@ test -d "${MY_PATH}/.git" || exit 1
 test -f "${MY_PATH}/reset-submodules.sh" || exit 1
 rm -rf "${MY_PATH}/.git/modules/"*
 rm -rf "${MY_PATH}/vim-"*
+rm -rf "${MY_PATH}/restricted-submodules/vim-"*
 rm -rf "${MY_PATH}/ansible-vim"
 rm -rf "${MY_PATH}/tlib_vim"
