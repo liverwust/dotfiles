0a609b2099a61cbb0bdfd9489be5df930028ecd6
Louis Wust <louis.w.wust@nasa.gov>
2025-05-29 10:11:45 -0400

    Turn off all Neovide animations

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -277,8 +277,15 @@ end
 -- BEGIN Neovide specific configuration
 
 if vim.g.neovide then
-  -- I mean, it's cool, but a bit distracting...
-  vim.g.neovide_cursor_animation_length = 0
+  -- I mean, animations are cool, but a bit distracting...
+  -- https://neovide.dev/faq.html#how-to-turn-off-all-animations
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
 end
 
 -- END Neovide specific configuration
