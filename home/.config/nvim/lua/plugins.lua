a7b11dba0cc4ed4f4f207a984caa526a28b5b62c
Louis Wust <louis.w.wust@nasa.gov>
2025-09-08 22:58:13 -0400

    Reapply "Let's try out Harpoon2"
    This reverts commit 2a63c64b812be5bfafde3c12b154a52deb64fe72.

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,5 +1,10 @@
 local plugins = {
   "PProvost/vim-ps1",
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
   "altercation/vim-colors-solarized",
   "arouene/vim-ansible-vault",
   "chazmcgarvey/vim-mermaid",
