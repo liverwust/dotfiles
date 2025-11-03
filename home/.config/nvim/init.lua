09dbd93d3accf04753fdadeaf02a54e318fbfe7f
Louis Wust <louis.w.wust@nasa.gov>
2025-11-02 23:24:34 -0500

    Temporarily comment-out the powershell shells

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -200,18 +200,18 @@ end
 -- Windows-specific integration with PowerShell
 -- https://neo.vimhelp.org/options.txt.html#shell-powershell
 -- :help shell-powershell
-if vim.call('has', 'win32') then
-  if vim.fn['executable']('pwsh') then
-    vim.o.shell = 'pwsh'
-  else
-    vim.o.shell = 'powershell'
-  end
-  vim.o.shellcmdflag = "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-  vim.o.shellredir   = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  vim.o.shellpipe    = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-  vim.o.shellquote   = ""
-  vim.o.shellxquote  = ""
-end
+--if vim.call('has', 'win32') then
+--  if vim.fn['executable']('pwsh') then
+--    vim.o.shell = 'pwsh'
+--  else
+--    vim.o.shell = 'powershell'
+--  end
+--  vim.o.shellcmdflag = "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+--  vim.o.shellredir   = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+--  vim.o.shellpipe    = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+--  vim.o.shellquote   = ""
+--  vim.o.shellxquote  = ""
+--end
 
 -- Set up my preferred colorscheme
 if vim.o.termguicolors then
